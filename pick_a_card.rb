cde032fb1f1561138cf14081a1dac74213b947e3
eugenebold <86603079+eugenebold@users.noreply.github.com>
2021-07-05 18:07:17 +0300

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A' ]
-suits = ['&diamonds;', '&hearts;', '&clubs;', '&spades;'] # Буби, Черви, Крести, Пики
+suits = ['Diamonds ♦', 'Hearts ♥', 'Clubs ♣', 'Spades ♠'] # Буби, Черви, Крести, Пики
 
 puts "#{values.sample} of #{suits.sample} "
